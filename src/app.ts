b7d3b853c2430bb34f604a4e036b3b94ff2c18ca
nahuel <ng0767185@gmail.com>
2026-05-21 04:01:55 -0300

    bugs arreglados

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(router)
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '../src/views'))
 app.set('view engine', 'ejs')
 
 app.get('/', async (req, res) => {
